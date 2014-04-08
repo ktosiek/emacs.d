6ce60b88001cc6cdf972e5bf867926fc442929ac
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-04-08 08:18:33 +0200

    safe-local-variable-values: encoding: utf-8

--- init.el
+++ init.el
@@ -130,6 +130,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("9fd20670758db15cc4d0b4442a74543888d2e445646b25f2755c65dcd6f1504b" default)))
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
