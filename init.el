c3e4b0d5db1a24b4d05afdd23a8408d82a12ca5c
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-04-09 22:04:07 +0200

    Ustaw uniquify

--- init.el
+++ init.el
@@ -138,7 +138,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("9fd20670758db15cc4d0b4442a74543888d2e445646b25f2755c65dcd6f1504b" default)))
  '(safe-local-variable-values (quote ((encoding . utf-8))))
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(uniquify-trailing-separator-p t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
